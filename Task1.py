da7685f8a5b3b0aa2b2c023fbdf60f634186731e
Ajay030606 <ajayjaiswal426850@gmail.com>
2025-09-05 16:27:08 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,8 +1,11 @@
 def func(x):
-     if(x==0):
+     if x < 0:
+         return "not defined for negative numbers"
+     elif(x==0):
          return 1
      else:
          return x * func(x-1)
 
 x=int(input("Enter a number: "))
 print("Factorial of ",x,"is",func(x))
+
